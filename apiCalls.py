2ce7bed85032dea4ae276d4d4c32bfb4700b8648
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-05-11 23:20:13 -0700

    testing gantt library

---
+++ apiCalls.py
@@ -0,0 +1,25 @@
+import datetime
+import gantt
+
+
+class Schedule:
+
+    def __init__(self):
+        gantt.define_font_attributes(fill='black', stroke='black',   stroke_width=0, font_family="Verdana")
+        self.chart = None
+        self.tasks = {}
+
+    def create_gantt(self, title):
+        self.chart = gantt.Project(name=title)
+
+    def add_new_task(self, name, start, duration, dependencies=[]):
+
+        if len(dependencies) > 0:
+            task = gantt.Task(name=name, start=start, duration=duration, depends_of=dependencies)
+        else:
+            task = gantt.Task(name=name, start=start, duration=duration)
+        self.chart.add_task(task)
+        self.tasks[name] = task
+
+    def create_svg(self, filename, today_date):
+        self.chart.make_svg_for_tasks(filename=filename, today=today_date)
---
+++ main.py
@@ -0,0 +1,10 @@
+import datetime
+from apiCalls import Schedule
+
+test1 = Schedule()
+test1.create_gantt('Test1')
+test1.add_new_task('T1', datetime.date(2020, 11, 20), 6)
+test1.add_new_task('T2', datetime.date(2020, 11, 26), 3, [test1.tasks.get('T1')])
+test1.add_new_task('T2', datetime.date(2020, 1, 29), 2, [test1.tasks.get('T1'), test1.tasks.get('T2')])
+test1.create_svg('test1.svg', datetime.date(2020, 11, 20))
+
