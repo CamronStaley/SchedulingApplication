53f89203aef0f68a54c3c098e8e5a7691e0681cf
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-05-13 20:33:41 -0700

    table class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-from apiCalls import Schedule
+from apiCalls import *
+from table import *
 
 test1 = Schedule()
 test1.create_gantt('Test1')
@@ -8,3 +8,6 @@ test1.add_new_task('T2', datetime.date(2020, 11, 26), 3, [test1.tasks.get('T1')]
 test1.add_new_task('T2', datetime.date(2020, 1, 29), 2, [test1.tasks.get('T1'), test1.tasks.get('T2')])
 test1.create_svg('test1.svg', datetime.date(2020, 11, 20))
 
+temp = Table()
+temp.create_table(3)
+temp.display()
---
+++ table.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+
+
+class Table:
+
+    def __init__(self):
+        self.fields = []
+        self.master = tk.Tk()
+        tk.Label(self.master, text="Task").grid(row=0, column=0)
+        tk.Label(self.master, text="Date").grid(row=0, column=1)
+        tk.Label(self.master, text="Dependencies").grid(row=0, column=2)
+        self.rows = 1
+
+    def display_data(self):
+        for row in self.fields:
+            print()
+            for data in row:
+                print(data.get(), end=' ')
+
+    def create_table(self, num_tasks):
+        self.rows += num_tasks
+        for x in range(1, self.rows):
+            e1 = tk.Entry(self.master)
+            e2 = tk.Entry(self.master)
+            e3 = tk.Entry(self.master)
+            e1.grid(row=x, column=0)
+            e2.grid(row=x, column=1)
+            e3.grid(row=x, column=2)
+            temp = [e1, e2, e3]
+            self.fields.append(temp)
+        tk.Button(self.master, text='Quit', command=self.master.quit).grid(row=self.rows + 1, column=0, sticky=tk.W,
+                                                                           pady=4)
+        tk.Button(self.master, text='Show', command=self.display_data).grid(row=self.rows + 1, column=1, sticky=tk.W,
+                                                                            pady=4)
+        self.rows += 1
+
+    def display(self):
+        self.master.mainloop()
